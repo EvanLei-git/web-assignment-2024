7ecad36dd516117c79d04a32d4bf0d7650288784
Evan Lei <71707767+EvanLei-git@users.noreply.github.com>
2024-06-13 16:49:08 +0300

    detailed server-side Checks & bttr post response

--- app.js
+++ app.js
@@ -38,18 +38,37 @@ db.serialize(() => {
 });
 
 app.get('/books', (req, res) => {
-    res.json({success: true, message: 'GET /books worked successfully' });
+    res.json({success: true, message: 'GET /books opened successfully' });
   });
   
 app.post('/books', (req, res) => {
     
     const { author, title, genre, price } = req.body;
 
-    // Input checks
-    if (!author || !title || !genre || !price || isNaN(price)) {
-        return res.status(400).json({ error: 'Please fill all fields correctly.' });
-    }
+    const genreList = [
+        'Science fiction',
+        'Satire',
+        'Drama',
+        'Action and Adventure',
+        'Romance',
+        'Mystery',
+        'Horror'
+    ];
 
+    // Input checks
+    if (!author || author.length > 25) {
+        return res.status(400).json({ error: 'Author must be filled and not exceed 25 characters.' });
+    }
+    else if (!title || title.length > 40) {
+        return res.status(400).json({ error: 'Title must be filled and not exceed 40 characters.' });
+    }
+    else if (!genre || !genreList.includes(genre)) {
+        return res.status(400).json({ error: 'Genre '+genre+' is not part of the allowed genres' });
+    }
+    else if (!price || isNaN(price)) {
+        return res.status(400).json({ error: 'Please only type a number for the Price.' });
+    }
+    
     // Insert data into the database
     const addToDataBase = db.prepare("INSERT INTO books (author, title, genre, price) VALUES (?, ?, ?, ?)");
     addToDataBase.run(author, title, genre, price, function (err) {
@@ -58,7 +77,7 @@ app.post('/books', (req, res) => {
         }
         const data = { id: this.lastID, author, title, genre, price };
         console.log(`${data.id}, ${data.author}, ${data.title}, ${data.genre}, ${data.price}`);
-        res.json({book: data });
+        res.json({success: true, id: data.id, title: data.title });
     });
     addToDataBase.finalize();
 });
